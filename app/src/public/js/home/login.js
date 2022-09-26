f2a754214af4c534ac8867cde585583c484f0900
hiwaypark <hiwaypark@gmail.com>
2022-09-26 19:35:40 +0900

    login page

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,16 @@
 "use strict"
+
+const userID = document.querySelector("#userID");
+const userPassword = document.querySelector("#userPassword");
+const loginBtn = document.querySelector("button"); 
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        userID: userID.value,
+        userPassword: userPassword.value
+    };  
+
+    console.log(req);
+}
